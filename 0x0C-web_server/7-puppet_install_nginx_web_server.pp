eb771e4c32011407df8298f1bf0dcb43f2cbc291
Agyemang73 <owusukevin17@gmail.com>
2022-09-21 12:46:31 +0100

    web

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,20 @@
+# Install and configure an Nginx server
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'line':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=hdZUCjAQaGw permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
